ca1166d99b4dfa2acf7513591e62773c0e936ad4
DanielFlores1223 <dany-232000@hotmail.com>
2023-04-14 14:05:04 -0600

    Project started

---
+++ README.md
@@ -0,0 +1,34 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Teslo API
+
+1. Git clone ...
+2. Execute
+
+```
+yarn install
+```
+
+3. Have Nest CLI installed
+
+```
+npm i -g @nestjs/cli
+```
+
+4. Up database
+
+```
+docker-compose up -d
+```
+
+5. Clone **.env.template** file and rename it to **.env**
+
+6. fill the environment variables defined in the **.env**
+
+7. Run API
+
+```
+yarn start:dev
+```
---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true, // Cargar las entidades automaticante
+      synchronize: true, // Solo se recomienda en desarrollo, en producción deberia estar en false, SIRVE: sincronizar los cambios de las entidades con las tablas
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
